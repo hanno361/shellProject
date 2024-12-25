62e1da0cb6eed25471d099061a5af64596698b25
unknown <ahmet.erginoz@ogr.sakarya.edu.tr>
2024-12-25 11:52:16 +0300

    Giriş ve çıkış yönlendirme işlemi eklendi.

--- myshell.c
+++ myshell.c
@@ -1,5 +1,5 @@
 /*
- * Author: [Grup Üyeleri: Kadir Ataç, Murat Yılmaz, Ömer Asaf Balıkçı]
+ * Author: [Grup Üyeleri: Kadir Ataç, Murat Yılmaz, Ömer Asaf Balıkçı, Ahmet Safa]
  * Proje İsmi: İşletim Sistemleri Ödevi - Basit Bir Komut Satırı Yorumlayıcısı
  * Açıklama: Bu proje, kullanıcının girdiği komutları analiz eden ve çalıştıran bir komut satırı yorumlayıcısını simüle eder.
  * Tarih: 24.12.2024
@@ -45,5 +45,35 @@ void execute_command(char *command) {
         }
     } else {
         perror("Fork başarısız");
+    }
+}
+
+// Giriş ve çıkış yönlendirme işleme
+void handle_redirection(char *command) {
+    char *input_file = strstr(command, "<");
+    char *output_file = strstr(command, ">");
+
+    if (input_file) {
+        *input_file = '\0'; // "<" kısmını kes
+        input_file = strtok(input_file + 1, " \t\n");
+        int fd_in = open(input_file, O_RDONLY);
+        if (fd_in < 0) {
+            perror("Giriş dosyası bulunamadı");
+            exit(1);
+        }
+        dup2(fd_in, STDIN_FILENO);
+        close(fd_in);
+    }
+
+    if (output_file) {
+        *output_file = '\0'; // ">" kısmını kes
+        output_file = strtok(output_file + 1, " \t\n");
+        int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd_out < 0) {
+            perror("Çıkış dosyası açılamadı");
+            exit(1);
+        }
+        dup2(fd_out, STDOUT_FILENO);
+        close(fd_out);
     }
 }
